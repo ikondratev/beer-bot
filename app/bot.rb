9b2aabf2267f1c5d5667ddb2cbe4d00f1fe82346
kilya <ikondratev@wirecapital.com>
2021-11-09 21:43:57 +0300

    [BB-0003] Add logic for botapi

--- app/bot.rb
+++ app/bot.rb
@@ -19,7 +19,8 @@ actions = Services::Bot::Actions.new(BOT_ACTIONS)
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     if BOT_ACTIONS.include?(message.text)
-      actions.call(message: message)
+      response = actions.call(message: message)
+      bot.api.send_message(response)
     else
       bot.api.send_message(chat_id: message.chat.id, text: "Actions is not confirmed")
     end
--- services/bot/actions.rb
+++ services/bot/actions.rb
@@ -6,7 +6,23 @@ module Services
       end
 
       def call(message:)
-        nil
+        response = store_message(message)
+        prepare_response(message, response)
+      end
+
+      private
+
+      def store_message(message)
+        case message.text
+        when "/hello" then "Hello #{message.from.first_name}"
+        when "/start" then "Let's start #{message.from.first_name}"
+        when "/end"   then  "Buy #{message.from.first_name}"
+        end
+      end
+
+      def prepare_response(message, text)
+        { chat_id: message.chat.id,
+          text: text }
       end
     end
   end
