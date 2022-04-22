1c2b0d8b912f809015ef4ce00b531a68950dcb23
kilya <ilya.kondratyev@kupibilet.ru>
2022-04-22 21:47:21 +0200

    [0023] Fix ci

--- db/migrate/001_add_model.rb
+++ db/migrate/001_add_model.rb
@@ -1,13 +1,14 @@
 class AddModel < ActiveRecord::Migration[7.0]
   class << self
     def up
-      create_table :model do |t|
+      create_table :user_model do |t|
         t.column :name, :string, :null => true
         t.column :lastname, :string, :null => true
         t.column :email, :string, :null => true
         t.column :actions, :string, :null => true
         t.column :gender, :string, :null => true
         t.column :age, :integer, :null => true
+        t.column :token, :integer, :null => true
       end
     end
 
